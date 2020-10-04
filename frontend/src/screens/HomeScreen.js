c468233b5371d9d7e52f43c9560009f58089aac8
rickyriosp <riki.rios91@gmail.com>
2020-10-04 11:32:54 -0400

    rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,7 +10,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} m={6} lg={4} xl={3} key={product._id}>
+          <Col key={product._id} sm={12} m={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
