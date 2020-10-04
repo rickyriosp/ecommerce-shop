5261a136598a75f6c60eaf424ceedbf3737c0d94
rickyriosp <riki.rios91@gmail.com>
2020-10-04 09:38:43 -0400

    project layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import React from 'react';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Welcome To ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
+import './bootstrap.min.css';
+import './index.css';
+
+import * as serviceWorker from './serviceWorker';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
