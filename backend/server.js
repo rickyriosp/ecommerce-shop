773c00bf0dd5b820f03aa66cdcc091c24f0535b4
rickyriosp <riki.rios91@gmail.com>
2020-10-04 22:18:16 -0400

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,32 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-  next();
-});
+// app.get('/', (req, res) => {
+//   res.send('API is running...');
+// });
 
 app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ frontend/build/precache-manifest.1bfd9225ca8b3f3ca7c37a5bbabae936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151d0e9846bbf887dfb6a7964c9329b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "567c83c189c0d21e9956",
+    "url": "/static/css/main.b4025ac1.chunk.css"
+  },
+  {
+    "revision": "77f50735e44bbcc146dc",
+    "url": "/static/js/2.033035a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.033035a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567c83c189c0d21e9956",
+    "url": "/static/js/main.a601f7f3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
