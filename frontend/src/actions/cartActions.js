63f1a411fceb67d9c4f0eda0e979f3a10c323511
rickyriosp <riki.rios91@gmail.com>
2020-10-08 23:58:35 -0400

    cart reducer & action

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,20 @@
+import { CART_ADD_ITEM } from '../reducers/cartReducers.js';
+import axios from 'axios';
+
+export const addItem = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: product.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
